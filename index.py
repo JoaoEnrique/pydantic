ecafe4ba2e73c0863ebd06a419fb3b12b16ef82e
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-03-29 11:39:18 -0300

    feat: exemplo de validação com pydantic

--- index.py
+++ index.py
@@ -9,14 +9,24 @@ usuario = {
     email: "joao@gmail.com"
 }
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 class User(BaseModel):
     name: str
     idade: int
     email: str
 
+    @field_validator("email")
+    def validate_email(cls, value):
+        if "@" not in value:
+            raise ValueError("Email Invalido")
+        return value
+
+
 user = User(name="Joao", idade=19, email="joao@gmail.com")
+
+#erros de validacao
+# user = User(name="Joao", idade=19, email="joaogmail.com") # causa erro por conta do validate_email (email não possui @)
 # user = User(name="Joao", idade="oi", email="joao@gmail.com") #causa erro por conta da validação de int da idade
 
 print(user)
